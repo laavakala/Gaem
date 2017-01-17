d63226862f791cf84f45078d4236283ce5b41afc
Laavis <laavakala@gmail.com>
2017-01-17 11:22:24 +0200

    moving platforms script started

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -3,39 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class Controller2D : MonoBehaviour {
-
-    public LayerMask collisionMask;
-
-    const float skinWidth = .015f;
-    public int horizontalRayCount = 4;
-    public int verticalRayCount = 4;
+public class Controller2D : RaycastController {
 
     float maxClimbAngle = 80;
     float maxDescentAngle = 80;
 
-
-    float horizontalRaySpacing;
-    float verticalRaySpacing;
-
-
-    BoxCollider2D collider;
-    RaycastOrigins raycastOrigins;
     public CollisionInfo collisions;
 
-    void Start(){
-        collider = GetComponent<BoxCollider2D>();
-        CalculateRaySpacing();
+    //when the start method is called, first call Raycast Controller's start method then continues this start method
+    public override void Start()
+    {
+        base.Start();
     }
- 
 
     public void Move(Vector3 velocity){
         UpdateRaycastOrigins();
 
         collisions.Reset();
         collisions.velocityOld = velocity;
-
-
 
             if (velocity.y < 0) {
             DescentSlope(ref velocity);
@@ -177,32 +162,6 @@ public class Controller2D : MonoBehaviour {
         }
     }
 
-    void UpdateRaycastOrigins(){
-        Bounds bounds = collider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        raycastOrigins.bottomLeft=new Vector2(bounds.min.x,bounds.min.y);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-
-    }
-
-    void CalculateRaySpacing(){
-        Bounds bounds = collider.bounds;
-        bounds.Expand(skinWidth * -2);
-
-        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-        verticalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-
-        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
-    }
-
-    struct RaycastOrigins{
-        public Vector2 topLeft, topRight;
-        public Vector2 bottomLeft, bottomRight;
-    }
 
     public struct CollisionInfo
     {
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : RaycastController {
+
+    public LayerMask passengerMask;
+    public Vector3 move;
+
+    // Use this for initialization
+    public override void Start() {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        UpdateRaycastOrigins();
+        Vector3 velocity = move * Time.deltaTime;
+
+        MovePassengers(velocity);
+        transform.Translate(velocity);
+
+    }
+    void MovePassengers(Vector3 velocity) {
+        HashSet<Transform> movedPassengers = new HashSet<Transform>();
+
+        float directionX = Mathf.Sign(velocity.x);
+        float directionY = Mathf.Sign(velocity.y);
+
+        //vertically moving platform
+        if (velocity.y != 0) {
+            float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+            for (int i = 0; i < verticalRayCount; i++) {
+                Vector2 RayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+                RayOrigin += Vector2.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(RayOrigin, Vector2.up * directionY, rayLength, passengerMask);
+
+                if (hit) {
+                    if (!movedPassengers.Contains(hit.transform)) {
+                        movedPassengers.Add(hit.transform);
+                    float pushX = (directionY == 1) ? velocity.x : 0;
+                        float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
+
+                        hit.transform.Translate(new Vector3(pushX, pushY));
+                    }
+                }
+            }
+        }
+    }
+}
