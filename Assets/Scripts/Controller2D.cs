c5747eeafa4187e500a0970ae7cd965107a347be
Laavis <laavakala@gmail.com>
2017-01-17 02:13:26 +0200

    initial commit

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,233 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    public LayerMask collisionMask;
+
+    const float skinWidth = .015f;
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    float maxClimbAngle = 80;
+    float maxDescentAngle = 80;
+
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+
+
+    BoxCollider2D collider;
+    RaycastOrigins raycastOrigins;
+    public CollisionInfo collisions;
+
+    void Start(){
+        collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+ 
+
+    public void Move(Vector3 velocity){
+        UpdateRaycastOrigins();
+
+        collisions.Reset();
+        collisions.velocityOld = velocity;
+
+
+
+            if (velocity.y < 0) {
+            DescentSlope(ref velocity);
+        }
+        if (velocity.x != 0){
+            HorizontalCollisions(ref velocity);
+        }
+        if (velocity.y != 0){
+            VerticalCollisions(ref velocity);
+        }
+        transform.Translate(velocity);
+    }
+
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 RayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            RayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(RayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            Debug.DrawRay(RayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            //if raycast hits something set y velocity equal the amount we have to move wtf no comments
+            if (hit)
+            {
+                //if raycast detecs slope on the ground execute void ClimbSlope. Cool stuff no idea
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+                if (i == 0 && slopeAngle<= maxClimbAngle){
+                    if (collisions.descendingSlope) {
+                        collisions.descendingSlope = false;
+                        velocity = collisions.velocityOld;
+                    }
+                    float distanceToSlopeStart = 0;
+                    //When ClimbSlope method is called uses velocity x once player has actually reached he slope 
+                    if (slopeAngle != collisions.slopeAngleOld){
+                        distanceToSlopeStart = hit.distance-skinWidth;
+                        velocity.x -= distanceToSlopeStart * directionX;
+                    }
+                    ClimbSlope(ref velocity, slopeAngle);
+                    velocity.x+=distanceToSlopeStart*directionX;
+                }
+
+                if (!collisions.climbingSlope || slopeAngle > maxClimbAngle){
+                    velocity.x = (hit.distance - skinWidth) * directionX;
+                    rayLength = hit.distance;
+
+                    //Fixes the funny jiggling movement on y axis
+                    if (collisions.climbingSlope){
+                        velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+                    }
+
+                    collisions.left = directionX == -1;
+                    collisions.right = directionX == 1;
+                }
+            }
+        }
+    }
+    void VerticalCollisions(ref Vector3 velocity){
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++){
+            Vector2 RayOrigin = (directionY==-1)?raycastOrigins.bottomLeft:raycastOrigins.topLeft;
+            RayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(RayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(RayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit){
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+
+                //fixes funny jiggling movement on x axis
+                if (collisions.climbingSlope){
+                    velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+                }
+
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+            }
+        }
+        if (collisions.climbingSlope){
+            float directionX = Mathf.Sign(velocity.x);
+            rayLength = Mathf.Abs(velocity.x) + skinWidth;
+            Vector2 rayOrigin = ((directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight) + Vector2.up * velocity.y;
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            if (hit) {
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if (slopeAngle != collisions.slopeAngle){
+                    velocity.x = (hit.distance - skinWidth) * directionX;
+                    collisions.slopeAngle = slopeAngle;
+                }
+            }
+        }
+    }
+    //Enables the player move along the slopes smoothly. Also calculates new speed new velocity for x and y axis 
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle){
+        float moveDistance = Mathf.Abs(velocity.x);
+        float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+        //enable jumping on slope
+        if(velocity.y <= climbVelocityY){
+            velocity.y = climbVelocityY;
+            velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+            collisions.below = true;
+            collisions.climbingSlope = true;
+            collisions.slopeAngle = slopeAngle;
+        }
+        
+    }
+
+    void DescentSlope(ref Vector3 velocity){
+        float directionX = Mathf.Sign(velocity.x);
+        Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity, collisionMask);
+
+        if (hit) {
+            float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+            if (slopeAngle !=0 && slopeAngle <= maxDescentAngle){
+                if (Mathf.Sign(hit.normal.x)== directionX){
+                    if(hit.distance - skinWidth <= Mathf.Tan(slopeAngle*Mathf.Deg2Rad)* Mathf.Abs(velocity.x)){
+                        float moveDistance = Mathf.Abs(velocity.x);
+                        float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+                        velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+                        velocity.y -= descendVelocityY;
+
+                        collisions.slopeAngle = slopeAngle;
+                        collisions.descendingSlope = true;
+                        collisions.below = true;
+                    }
+                }
+            }
+        }
+    }
+
+    void UpdateRaycastOrigins(){
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft=new Vector2(bounds.min.x,bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+
+    }
+
+    void CalculateRaySpacing(){
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    struct RaycastOrigins{
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public bool climbingSlope;
+        public bool descendingSlope;
+        public float slopeAngle, slopeAngleOld;
+        public Vector3 velocityOld;
+
+        
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+            climbingSlope = false;
+            descendingSlope = false;
+           
+
+            slopeAngleOld = slopeAngle;
+            slopeAngle = 0;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+
+    void Start()
+    {
+        Physics2D.gravity = new Vector2(0.0f, -9.8f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Physics2D.gravity = new Vector2(0.0f, 9.8f);
+            Debug.Log("Gravity wooo");
+        }
+    }
+
+}
